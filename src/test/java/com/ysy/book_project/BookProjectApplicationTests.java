d268b7679be5ec1874046df727d4706587b35546
seyoon.yang <dosy13@naver.com>
2024-01-23 21:10:54 +0900

    1.0.0 first commit

---
+++ src/test/java/com/ysy/book_project/BookProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ysy.book_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
